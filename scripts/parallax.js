ec5858eead03e2124574fdd1b3014e94b4340ef8
Daniel Kretz <kretz@Daniels-MacBook-Pro.local>
2016-09-19 11:26:10 +0200

    adding parallax

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -1,22 +1,22 @@
-﻿// $(document).ready(function () {
-//     $('.parallax-box').each(function () {
-//         var bgobj = $(this);
+﻿$(document).ready(function () {
+    $('.parallax-box').each(function () {
+        var bgobj = $(this);
 
-//         $(window).scroll(function () {
+        $(window).scroll(function () {
 
-//             if ($(window).scrollTop() >= $(bgobj).position().top - $(window).height()) {
+            if ($(window).scrollTop() >= $(bgobj).position().top - $(window).height()) {
 
 
-//                 var o = $(window).scrollTop() - $(bgobj).position().top;
+                var o = $(window).scrollTop() - $(bgobj).position().top;
 
-//                 var yPos = o / bgobj.data('speed');
+                var yPos = o / bgobj.data('speed');
 
-//                 // Put together our final background position
-//                 var coords = '50% ' + yPos + 'px';
+                // Put together our final background position
+                var coords = '50% ' + yPos + 'px';
 
-//                 // Move the background
-//                 bgobj.css({ backgroundPosition: coords });
-//             }
-//         });
-//     });
-// });
+                // Move the background
+                bgobj.css({ backgroundPosition: coords });
+            }
+        });
+    });
+});
